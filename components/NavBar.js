9796278ac4370c48163399de46336784dd7329b0
신형기 <tkaqkeldk@hanatour.com>
2022-01-15 20:28:23 +0900

    [router] prerender

---
+++ components/NavBar.js
@@ -0,0 +1,17 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+export default function NavBar() {
+  const router = useRouter();
+  return (
+    <nav>
+      <Link href="/">
+        <a style={{ color: router.pathname === "/" ? "red" : "blue" }}>HOME</a>
+      </Link>
+      <Link href="/about">
+        <a style={{ color: router.pathname === "/about" ? "red" : "blue" }}>
+          ABOUT
+        </a>
+      </Link>
+    </nav>
+  );
+}
